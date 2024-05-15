57ff5e5c23bbdca1ac0110b11916c87c24995666
Jp Keys <jpmusik29@gmail.com>
2024-05-15 07:26:19 -0400

    Setting up star rating State

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import Star from "./Star";
 
 export default function StarRating({maxRating = 5}) {
+    const [rating, setRating] = useState(0)
     return (
         <div className="star" >
             <div className="star-indiv" >
                 {Array.from({length: maxRating}, (_, i) => <span key={i}><Star/></span>)}
             </div>
+            <p>
+                {rating || ''}
+            </p>
         </div>
     )
 }
