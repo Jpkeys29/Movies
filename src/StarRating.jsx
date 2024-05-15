c115ce9ddbef60c2f40f6681ba1594d183497ae0
Jp Keys <jpmusik29@gmail.com>
2024-05-15 07:46:24 -0400

    Changing the rating using a handleRating function

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -3,10 +3,14 @@ import Star from "./Star";
 
 export default function StarRating({maxRating = 5}) {
     const [rating, setRating] = useState(0)
+
+    function handleRating(rating){
+        setRating(rating)
+    }
     return (
         <div className="star" >
             <div className="star-indiv" >
-                {Array.from({length: maxRating}, (_, i) => <span key={i}><Star onRate={() => setRating(i+1)}  /></span>)}
+                {Array.from({length: maxRating}, (_, i) => <span key={i}><Star onRate={() => handleRating(i+1)}  /></span>)}
             </div>
             <p>
                 {rating || ''}
