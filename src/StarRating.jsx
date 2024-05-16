2a0710adef0a26a2a2fe0ade0eaa1849e699fe7c
Jp Keys <jpmusik29@gmail.com>
2024-05-16 18:36:49 -0400

    Star rating

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -5,15 +5,16 @@ export default function StarRating({maxRating = 5}) {
     const [rating, setRating] = useState(0)
 
     function handleRating(rating){
-        setRating(rating)
+        setRating(rating);
     }
     return (
         <div className="star" >
             <div className="star-indiv" >
-                {Array.from({length: maxRating}, (_, i) => <span key={i}><Star onRate={() => handleRating(i+1)}  /></span>)}
+                {Array.from({length: maxRating}, (_, i) => 
+                (<Star key={i} onRate={() => setRating(i+1)} full={rating >= i + 1}/>
+                ))}
             </div>
             <p>
-                {rating || ''}
             </p>
         </div>
     )
